d531f18d1eaaa3c458f0cc1c029911cb5afeff1c
CesarGBkR <yexotortuesprin@gmail.com>
2022-05-05 22:00:07 -0500

    Cleaning code

--- API/lib/tests/services/filterByCredits.test.js
+++ API/lib/tests/services/filterByCredits.test.js
@@ -5,7 +5,7 @@ describe('Test lab to Filter By Credits service', () => {
 
         const students = [{name:'student1', credits:500}, {name: 'student2', credits: 499}]
         const filterGreaterThan = filterByCredits.greaterThan(students, 500)
-        const filterSmallerThan = filterByCredits.smallerThan(students, 500)
+        const filterSmallerThan = filterByCredits.lessThan(students, 500)
         expect(filterGreaterThan).toContain('student1')
         expect(filterSmallerThan).toContain('student2')
     })
